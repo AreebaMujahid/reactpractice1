ae6f977699446925805b0619bb402ea964f6561e
Areeba Mujahid <89256134+AreebaMujahid@users.noreply.github.com>
2024-10-15 13:46:55 +0500

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//import seriesData from "src/api/data.json";
+import data from "./api/dataa.json";
 import { Fragment } from 'react';
+import {List} from "./components/List"
 export const App = () =>{
   return (
     <Fragment>
     <NetFlixSeries />
-    <NetFlixSeries />
-    <NetFlixSeries />
+    
+    
 
     </Fragment>
   
@@ -20,70 +21,15 @@ export const App = () =>{
 
   );
 };
-const NetFlixSeries = () =>{
-  const appIntro = "This is a gaming app";
-  const detail = "This app contains authentication system , movies recommendation on basis of user input , stores feedback of each movie from user , display each feedback to all users , rating list, some games;"
-  const name = "Horror Movie";
-  const ratings = 5;
-  const age = 16;
-  const returnGenre = () => {
-    const genre = "Horror";
-    return genre;
-
-
-  };
-  if (age < 18)
-  {  
-    return (
-    <div>
-      <div>
-        <img src="rename.jpeg"  width="40%" height = "40%" />
-      </div>
-      <div>
-        <h1>{appIntro}</h1>
-      </div>
-      <div>
-        <h2>{detail}</h2>
-      </div>
-      <div>
-        <h3>{returnGenre()}</h3>
-      </div>
-      <div>
-        <button>
-          Not available
-        </button>
-      </div>
-    </div>
-
-
-   );
-  }
-
+const NetFlixSeries = () => {
   return (
-    
-
-    <div>
-      <div>
-        <img src="rename.jpeg" width="40%" height = "40%" />
-      </div>
-      <div>
-        <h1>{appIntro}</h1>
-      </div>
-      <div>
-        <h2>{detail}</h2>
-      </div>
-      <div>
-        <h3>{returnGenre()}</h3>
-      </div>
-      <div>
-        <button>
-          watch now
-        </button>
-      </div>
-    </div>
-
-
+    <ul>
+      {data.map((curElem) => (
+        <List key={curElem.title} curElem={curElem}/>
+        
+      ))}
+    </ul>
   );
+};
 
-}
 export default App
